2c9137e09f5f689f83e8a420968a83fd1a882968
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2022-04-19 00:58:17 +0300

    Math issues final json and script

--- match_issues.py
+++ match_issues.py
@@ -1,3 +1,4 @@
+from distutils.command.clean import clean
 import json
 import requests
 import re
@@ -10,24 +11,32 @@ with open("issues_clean.json", 'r') as clean_json_file:
     data = json.load(clean_json_file)
 
 with open("issues_final.json", "w") as final_json_file: 
-    for i in range (5):
-        comments = data[i]['comments']
-        duplicates = []
+    final_json_file.write("[")
+    for currentObject in data:
+        comments = currentObject['comments']
         for comment in comments: 
-
             finds = re.search('((?:https:\/\/github.com\/flutter\/flutter\/issues\/)(\d+))', comment)
-            if(finds):
-                print(i, "link found", finds.group(2), comment)
-                a = requests.get(urlRoot + finds.group(2), auth=(username, token)).json()
-                print("link: ", urlRoot + finds.group(2))
-                print('karsilik', a['title'])
+            if(finds and int(finds.group(2)) > 0):
+                print("link found", finds.group(2), comment)
+                original = requests.get(urlRoot + finds.group(2), auth=(username, token)).json()
+                currentObject['originalId'] = original["number"]
+                currentObject['originalTitle'] = original["title"]
+                currentObject['originalBody'] = original["body"]
+                json.dump(currentObject, final_json_file, indent=4, separators=(',', ': '))
+                final_json_file.write(",\n")
                 break
             else:
                 finds = re.search('((?:#)(\d+))', comment)
-                if(finds):
-                    print(i, "number found", finds.group(2), comment)
+                if(finds and int(finds.group(2)) > 0):
+                    print("number found", finds.group(2), comment)
+                    original = requests.get(urlRoot + finds.group(2), auth=(username, token)).json()
+                    currentObject['originalId'] = original["number"]
+                    currentObject['originalTitle'] = original["title"]
+                    currentObject['originalBody'] = original["body"]
+                    json.dump(currentObject, final_json_file, indent=4, separators=(',', ': '))
+                    final_json_file.write(",\n")
                     break
-            
-finds = re.search('(?:#(\d+))|(https:\/\/github.com\/flutter\/flutter\/issues\/\d+)$', '#45655 https://github.com/flutter/flutter/issues/54565')
-print("last finds", finds.group())
-            
+    final_json_file.write("]")   
+
+clean_json_file.close()
+final_json_file.close()  
